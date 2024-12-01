9d46773bc56c80172fa7e8ad6c6950f675a34627
brayancb <143953434+brayancb@users.noreply.github.com>
2024-12-01 20:39:36 -0300

    cambios
    mejora de distancia de manos, iniciar juego apretando los 2 gatillos, circulo de carga

--- Assets/distanciaManos.cs
+++ Assets/distanciaManos.cs
@@ -1,17 +1,19 @@
+// distanciaManos.cs
 using UnityEngine;
 
 public class HandOffset : MonoBehaviour
 {
-    public Transform leftHand;  // Asigna manualmente el objeto de la mano izquierda desde el editor
-    public Transform rightHand; // Asigna manualmente el objeto de la mano derecha desde el editor
+    public Transform leftHand;  // Asigna la mano izquierda en el editor
+    public Transform rightHand; // Asigna la mano derecha en el editor
+    public Vector3 offset = new Vector3(0, 0, 0.2f); // Ajusta el desplazamiento según sea necesario
 
-    void Update()
+    void Start()
     {
-        // Mueve las manos hacia adelante en el eje Z local
-        leftHand.localPosition += new Vector3(0, 0, 6.6f);  // Ajusta el valor "0.2f" según tu necesidad
-        rightHand.localPosition += new Vector3(0, 0, 6.6f); // Ajusta el valor "0.2f" según tu necesidad
+        // Aplica el desplazamiento inicial a las manos
+        if (leftHand != null)
+            leftHand.localPosition += offset;
 
+        if (rightHand != null)
+            rightHand.localPosition += offset;
     }
-
-    
 }
